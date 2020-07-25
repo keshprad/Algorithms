2f8cfa8e935186055fac16ebca3f484b415cc8db
Rishi Keshav Pradeep <keshav.r.pradeep@gmail.com>
2020-07-25 10:16:04 -0700

    🧳 Changed test cases that are run

--- PrimMinSpanningTree/run_test.py
+++ PrimMinSpanningTree/run_test.py
@@ -19,6 +19,5 @@ runPrimAlg("test1.txt")
 runPrimAlg("test2.txt")
 runPrimAlg("test3.txt")
 runPrimAlg("test4.txt")
-# runPrimAlg("test5.txt")
-# commented because of test-case length
+runPrimAlg("test5.txt")
 runPrimAlg("edges.txt")
