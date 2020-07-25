75b3f77ead5b50c1fccd7e51dc0bccacc161a429
Rishi Keshav Pradeep <keshav.r.pradeep@gmail.com>
2020-07-25 10:17:11 -0700

    🐝 Changed Kruskal to use Union Find disjoint set and work more efficiently!

--- KruskalMinSpanningTree/KruskalAlg.py
+++ KruskalMinSpanningTree/KruskalAlg.py
@@ -1,12 +1,16 @@
+from UnionFind_DisjointSet import UnionFind
+
 class KruskalMST:
     def __init__(self, fileIn):
-        self.edges = self.read_file(fileIn)
+        # c_components will be a Union Find data structure to make tracking connected components simple.
+        # Allows us to think of connected components as clusters
+        self.edges, self.c_components = self.read_file(fileIn)
         self.treeEdges = []
 
     def read_file(self, fileIn):
         fin = open(fileIn)
         numVertices, numEdges = None, None
-        edges = []
+        edges, c_components = [], []
 
         for line in fin:
             line = line.split()
@@ -14,36 +18,26 @@ class KruskalMST:
                 numVertices = int(line[0])
                 numEdges = int(line[1])
             else:
-                edges.append((int(line[0]), int(line[1]), int(line[2])))
-        return edges
+                node1, node2, weight = int(line[0]), int(line[1]), int(line[2])
+                edges.append((node1, node2, weight))
+                if [node1] not in c_components:
+                    c_components.append([node1])
+                if [node2] not in c_components:
+                    c_components.append([node2])
+        return edges, UnionFind.UnionFind(c_components)
 
     def findMinSpanningTree(self):
         sorted_edges, c_components, ovr_cost = self.sort_costs(), [], 0
 
         for edge in sorted_edges:
             if edge[0] != edge[1]:  # Don't count self-loops
-                edgeIsIn = self.checkCycles(c_components, edge)
+                edgeIsIn = self.checkCycles(edge)
 
-                # Adds the edge if the edge doesn't create a cycle
-                if len(edgeIsIn) != 2 or edgeIsIn[0] != edgeIsIn[1]:
+                # Adds the edge if it won't create a cycle
+                if edgeIsIn[0] != edgeIsIn[1]:
                     self.treeEdges.append(edge)
+                    self.c_components.union(edge[0], edge[1])
                     ovr_cost += edge[2]
-
-                # Each case where the edge doesn't cause a cycle
-                if len(edgeIsIn) == 2:
-                    # If the edge spans 2 connects 2 initially disconnected components
-                    if edgeIsIn[0] != edgeIsIn[1]:
-                        c_components[edgeIsIn[0]] += c_components[edgeIsIn[1]]
-                        c_components = c_components[:edgeIsIn[1]] + c_components[edgeIsIn[1] + 1:]
-                elif len(edgeIsIn) == 1:
-                    # If one edge or the other is in a connected component, while others aren't
-                    if edge[0] in c_components[edgeIsIn[0]]:
-                        c_components[edgeIsIn[0]].append(edge[1])
-                    if edge[1] in c_components[edgeIsIn[0]]:
-                        c_components[edgeIsIn[0]].append(edge[0])
-                elif len(edgeIsIn) == 0:
-                    # None of the edges are in a connected component
-                    c_components.append([edge[0], edge[1]])
         return self.treeEdges, ovr_cost
 
     def sort_costs(self, edges=None):
@@ -51,11 +45,8 @@ class KruskalMST:
             return sorted(self.edges, key=lambda x: x[2])
         return sorted(edges, key=lambda x: x[2], reverse=True)
 
-    def checkCycles(self, c_components, edge):
-        edgeIsIn = []
-        for i in range(len(c_components)):
-            if edge[0] in c_components[i]:
-                edgeIsIn.append(i)
-            if edge[1] in c_components[i]:
-                edgeIsIn.append(i)
+    def checkCycles(self, edge):
+        parent1 = self.c_components.find(edge[0])
+        parent2 = self.c_components.find(edge[1])
+        edgeIsIn = [parent1, parent2]
         return edgeIsIn
