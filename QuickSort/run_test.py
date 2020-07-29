fbe7492889f9600749c1b22af4a944687563e40e
Rishi Keshav Pradeep <keshav.r.pradeep@gmail.com>
2020-07-29 01:06:29 -0700

    ⏳. Quicksort compare pivot choice effects on running time

--- QuickSort/run_test.py
+++ QuickSort/run_test.py
@@ -1,3 +1,4 @@
+import time as t
 from QuickSort import Comparisons
 
 def runComparisons(fileIn):
@@ -11,7 +12,10 @@ def runComparisons(fileIn):
     compare.reset()
     print("using leftmost pivot:")
     print("\tinput = " + str(compare.res))
+    start = t.time()
     compare.quickSort(0, len(compare.input) - 1, 1)
+    end = t.time()
+    print("\tTime to sort using QuickSort: " + str(end-start))
     print("\tsorted = " + str(compare.res))
     print("\tcomparisons = " + str(compare.comparisons))
 
@@ -19,7 +23,10 @@ def runComparisons(fileIn):
     compare.reset()
     print("using rightmost pivot:")
     print("\tinput = " + str(compare.res))
+    start = t.time()
     compare.quickSort(0, len(compare.input) - 1, 2)
+    end = t.time()
+    print("\tTime to sort using QuickSort: " + str(end-start))
     print("\tsorted = " + str(compare.res))
     print("\tcomparisons = " + str(compare.comparisons))
 
@@ -27,7 +34,10 @@ def runComparisons(fileIn):
     compare.reset()
     print("using median-of-3 pivot:")
     print("\tinput = " + str(compare.res))
+    start = t.time()
     compare.quickSort(0, len(compare.input) - 1, 3)
+    end = t.time()
+    print("\tTime to sort using QuickSort: " + str(end-start))
     print("\tsorted = " + str(compare.res))
     print("\tcomparisons = " + str(compare.comparisons) + "\n")
 
