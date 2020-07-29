83fd4154a1673f24c555a777fbadc109e59dd0df
Rishi Keshav Pradeep <keshav.r.pradeep@gmail.com>
2020-07-29 01:15:55 -0700

    ⏰. Kruskal's Add timer for comparison with Prim's

--- KruskalMinSpanningTree/run_test.py
+++ KruskalMinSpanningTree/run_test.py
@@ -1,3 +1,4 @@
+import time as t
 from KruskalMinSpanningTree import KruskalAlg
 
 
@@ -8,8 +9,11 @@ def runKruskalAlg(fileIn):
     fileIn = "../PrimMinSpanningTree/testCases/" + fileIn
     Kruskal = KruskalAlg.KruskalMST(fileIn)
 
+    start = t.time()
     treeEdges, ovrCost = Kruskal.findMinSpanningTree()
+    end = t.time()
 
+    print("\tTime to run Kruskal's MST Alg: " + str(end - start))
     print("\tMinimum cost to span tree: " + str(ovrCost))
     print("\tTree Edges in the order spanned:")
     print("\t\t" + str(treeEdges) + "\n")
