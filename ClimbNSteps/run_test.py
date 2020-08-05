61ec0a70257e299f329b8ee9b14c69fa06383780
Rishi Keshav Pradeep <keshav.r.pradeep@gmail.com>
2020-08-04 20:59:10 -0700

    Update the run test for n step climber

--- ClimbNSteps/run_test.py
+++ ClimbNSteps/run_test.py
@@ -3,8 +3,9 @@ from ClimbNSteps import climb_n_steps
 
 def runClimbSteps(n, x):
     combos = climb_n_steps.find_step_combinations(n, x)
+
     print("There are " + str(combos) + " ways to climb up " + str(
-        n) + " steps if at each move you can choose the number of steps from " + str(x) + "!")
+        n) + " steps if at each move you can choose the number of steps from " + str(x) + "!\n")
 
 ######################
 runClimbSteps(5, [1, 2])
